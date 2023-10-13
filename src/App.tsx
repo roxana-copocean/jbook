970c0c13dd243ff93860b7272100601a7fa6f4f1
Roxana Copocean <roxana.mica@gmail.com>
2023-10-13 14:21:29 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ input, setInput ] = useState('');
+	const [ code, setCode ] = useState('');
+
+	const onClickHandler = () => {
+		console.log(input);
+	};
+	return (
+		<div>
+			<textarea onChange={(e) => setInput(e.target.value)} value={input} />
+			<div>
+				<button onClick={onClickHandler}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
 }
 
 export default App;
