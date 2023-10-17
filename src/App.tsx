4e6c49c65acf9bbc19b159a1117ab56221cb01c5
Roxana Copocean <roxana.mica@gmail.com>
2023-10-17 17:19:01 +0300

    ref added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
 	const [ input, setInput ] = useState('');
 	const [ code, setCode ] = useState('');
+	const ref = useRef<any>();
 
-	const onClickHandler = () => {
-		console.log(input);
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: '/esbuild.wasm'
+		});
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+	const onClickHandler = async () => {
+		if (!ref.current) {
+			return;
+		}
+		const result = await ref.current.transform(input, {
+			loader: 'jsx',
+			target: 'es2015'
+		});
+
+		setCode(result.code);
 	};
 	return (
 		<div>
