59c7d764d1cbff0e09db55e4b01bf6b6bf9909a1
Roxana Copocean <roxana.mica@gmail.com>
2023-10-19 17:01:52 +0300

    fix fetch plugin

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
 import localforage from 'localforage';
 
-// // Create a local cache using localforage for storing fetched files
+// Create a local cache using localforage for storing fetched files
 const fileCache = localforage.createInstance({
 	name: 'filecache'
 });
@@ -11,23 +11,43 @@ export const fetchPlugin = (inputCode: string) => {
 	return {
 		name: 'fetchPlugin',
 		setup(build: esbuild.PluginBuild) {
-			// Handle main file of a module
-			build.onResolve({ filter: /.*/ }, async (args: any) => {
+			build.onLoad({ filter: /(^index\.js$)/ }, () => {
 				return {
-					namespace: 'a',
-					path: `https://unpkg.com/${args.path}`
+					loader: 'jsx',
+					contents: inputCode
 				};
 			});
-			// Load modules using this plugin
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				// If the path is 'index.js', return the user's input code
-				if (args.path === 'index.js') {
-					return {
-						loader: 'jsx',
-						contents: inputCode
-					};
+
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+				// If the file is cached, return it immediately
+				if (cachedResult) {
+					return cachedResult;
 				}
 
+				// Fetch the file from the specified path using Axios
+				const { data, request } = await axios.get(args.path);
+
+				const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'");
+				const contents = `
+                        const style = document.createElement('style');
+                        style.innerText = '${escaped}'; 
+                        document.head.appendChild(style);
+                      `;
+
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents: contents,
+					resolveDir: new URL('./', request.responseURL).pathname
+				};
+				// Store the response in the cache
+				await fileCache.setItem(args.path, result);
+
+				return result;
+			});
+
+			// Load modules using this plugin
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				// Check if the file is already cached; if not, fetch it from the CDN
 				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 				// If the file is cached, return it immediately
@@ -36,9 +56,8 @@ export const fetchPlugin = (inputCode: string) => {
 				}
 				// Fetch the file from the specified path using Axios
 				const { data, request } = await axios.get(args.path);
-				const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
 				const result: esbuild.OnLoadResult = {
-					loader: loader,
+					loader: 'jsx',
 					contents: data,
 					resolveDir: new URL('./', request.responseURL).pathname
 				};
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -20,6 +20,13 @@ export const unpkgPathPlugin = () => {
 					path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
 				};
 			});
+			// Handle main file of a module
+			build.onResolve({ filter: /.*/ }, async (args: any) => {
+				return {
+					namespace: 'a',
+					path: `https://unpkg.com/${args.path}`
+				};
+			});
 		}
 	};
 };
