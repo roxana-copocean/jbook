ced413df594bb96541c335ac87484df49cf63ae7
Roxana Copocean <roxana.mica@gmail.com>
2023-10-24 16:55:02 +0300

    monaco editor configuration editor added

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-	return <MonacoEditor height="100px" />;
+	return (
+		<MonacoEditor
+			height="200px"
+			language="javascript"
+			theme="dark"
+			options={{
+				wordWrap: 'on',
+				minimap: { enabled: false },
+				showUnused: false,
+				folding: false,
+				lineNumbersMinChars: 3,
+				fontSize: 20,
+				scrollBeyondLastLine: false,
+				automaticLayout: true
+			}}
+		/>
+	);
 };
 
 export default CodeEditor;
