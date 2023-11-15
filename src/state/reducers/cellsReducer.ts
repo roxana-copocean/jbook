ed7a30d7b972b7fc06a78cfa3ef27315c82bcee1
Roxana Copocean <roxana.mica@gmail.com>
2023-11-15 16:58:47 +0200

    cellsreducer using immer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import { produce } from 'immer';
 import { ActionType } from '../actions-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -21,20 +22,34 @@ const initialState: CellsState = {
 };
 
 // Cells Reducer
-
-const cellReducer = (state: CellsState = initialState, action: Action): CellsState => {
+const cellReducer = produce((state: CellsState = initialState, action: Action) => {
 	switch (action.type) {
+		// update cell using immer
 		case ActionType.UPDATE_CELL:
-			return state;
+			state.data[action.payload.id].content = action.payload.content;
+			return;
+		// delete cell using immer
 		case ActionType.DELETE_CELL:
-			return state;
+			delete state.data[action.payload];
+			state.order = state.order.filter((id) => id !== action.payload);
+			return;
+		// move cell using immer
 		case ActionType.MOVE_CELL:
-			return state;
+			const { direction } = action.payload;
+			const index = state.order.findIndex((id) => id === action.payload.id);
+			const targetIndex = direction === 'up' ? index - 1 : index + 1;
+			if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+				return;
+			}
+			state.order[index] = state.order[targetIndex];
+			state.order[targetIndex] = action.payload.id;
+			return;
+		// insert cell before using immer
 		case ActionType.INSERT_CELL_BEFORE:
-			return state;
+			return;
 		default:
 			return state;
 	}
-};
+});
 
 export default cellReducer;
