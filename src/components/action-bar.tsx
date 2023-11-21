cf95b5878412f4079bc5adacd2d6ed5035498349
Roxana Copocean <roxana.mica@gmail.com>
2023-11-21 16:34:52 +0200

    action bar css

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './action-bar.css';
 import { useActions } from '../hooks/use-actions';
 
 interface ActionBarProps {
@@ -7,7 +8,7 @@ interface ActionBarProps {
 const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
 	const { moveCell, deleteCell } = useActions();
 	return (
-		<div>
+		<div className="action-bar">
 			<button className="button is-primary is-small" onClick={() => moveCell(id, 'up')}>
 				<span className="icon">
 					<i className="fas fa-arrow-up" />
